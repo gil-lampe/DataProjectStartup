8c94e727fee5310df235a707bc1b44018f5006ee
miacoursera <miacoursera@gmail.com>
2017-01-29 20:19:11 +0100

    debut ecriture python pour automatiser la recherche

--- techcrunch data/techcrunch.py
+++ techcrunch data/techcrunch.py
@@ -1,26 +1,26 @@
 import numpy
 import xlsxwriter
+import sys
+import word2vec
+import nltk
+nltk.download()
 # Create a workbook and add a worksheet.
-workbook = xlsxwriter.Workbook('techcrunch.xlsx')
-worksheet = workbook.add_worksheet()
+#workbook = xlsxwriter.Workbook('techcrunch.xlsx')
+#worksheet = workbook.add_worksheet()
 
-# Some data we want to write to the worksheet.
-expenses = (
-    ['Rent', 1000],
-    ['Gas',   100],
-    ['Food',  300],
-    ['Gym',    50],
-)
+# open data
+tokenizer = nltk.data.load("3-gadgets.txt")
 
+print(tokenizer)
 
 # Start from the first cell. Rows and columns are zero indexed.
 row = 0
 col = 0
 
 # Iterate over the data and write it out row by row.
-for item, cost in (expenses):
-    worksheet.write(row, col,     item)
-    worksheet.write(row, col + 1, cost)
-    row += 1
+#for item, cost in (a):
+ #   worksheet.write(row, col,     item)
+  #  worksheet.write(row, col + 1, cost)
+   # row += 1
 
-workbook.close()
+#workbook.close()
